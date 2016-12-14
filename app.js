8dd0094a4ee63beaf042e97c398fc797e69ceb10
ramesh <ramesh@nirvaha.com>
2016-12-13 17:30:28 -0800

    text change

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var port = require('./port');
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello Yall\n");
+  response.end("Hello world\n");
 });
 
 // server.listen(port.number);
