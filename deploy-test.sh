d20b0578dd5eeaced33e2313d92382790c0e9fca
ramesh <ramesh@nirvaha.com>
2017-01-14 11:20:41 -0800

    added test scripts

--- deploy-test.sh
+++ deploy-test.sh
@@ -1,27 +1,10 @@
-startingPort=8000
-buildNumber=${BUILD_NUMBER}
-port=$((startingPort + buildNumber))
-version=${BUILD_ID}
 #marathon="35.163.154.240"
 marathon=$(curl http://myip.dnsomatic.com)
 echo ${marathon}
 
-echo Deleting
-# destroy old application
-curl -X DELETE -H "Content-Type: application/json" http://${marathon}:8080/v2/apps/app?force=true 
- 
-echo sleeping
+echo Deleting test
+curl -X DELETE -H "Content-Type: application/json" http://${marathon}:8080/v2/apps/microservice2-test?force=true 
 sleep 5 
- 
-echo creating app json
-
-# these lines will create a copy of app_marathon.json and update the image version
-cp -f app_marathon.json app_marathon.json.tmp
-sed -i "s/latest/working/g" app_marathon.json.tmp
- 
 echo posting app
-
-# post the application to Marathon
-curl -X POST -H "Content-Type: application/json" http://${marathon}:8080/v2/apps -d@app_marathon.json.tmp
-
-echo DOne...
+curl -X POST -H "Content-Type: application/json" http://${marathon}:8080/v2/apps -d@app_marathon_test.json
+echo Deployed to test
--- push.sh
+++ push.sh
@@ -1,1 +1,8 @@
-docker push localhost:5000/containersol/nodejs_app:working
+buildNumber=${BUILD_NUMBER}
+
+docker push localhost:5000/devopsly200-microservice1-teststage
+docker push localhost:5000/devopsly200-microservice1-teststage-failed
+docker push localhost:5000/devopsly200-microservice1-stagingstage
+docker push localhost:5000/devopsly200-microservice1-stagingstage-failed
+docker push localhost:5000/devopsly200-microservice1-productionstage
+docker push localhost:5000/devopsly200-microservice1-productionstage-failed
