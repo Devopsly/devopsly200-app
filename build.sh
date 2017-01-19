48425c32aac04be36d21b212a6579a7fc460fff5
ramesh <ramesh@nirvaha.com>
2017-01-18 20:50:05 -0800

    misc

--- build.sh
+++ build.sh
@@ -9,12 +9,3 @@ docker build -t localhost:5000/devopsly200-app-stagingstage -f DockerfileStaging
 docker rmi -f localhost:5000/devopsly200-app-productionstage
 docker build -t localhost:5000/devopsly200-app-productionstage -f DockerfileProduction .
 
-marathon=$(curl http://myip.dnsomatic.com)
-echo ${marathon}
-
-echo Deleting cadvisor 
-curl -X DELETE -H "Content-Type: application/json" http://${marathon}:8080/v2/apps/cadvisor?force=true
-sleep 5
-echo posting app
-curl -X POST -H "Content-Type: application/json" http://${marathon}:8080/v2/apps -d@cadvisor_marathon.json
-echo Deployed to test
