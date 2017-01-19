10dceddb4e2b795954a68c9cff7c664ca4806480
ramesh <ramesh@nirvaha.com>
2017-01-18 19:59:38 -0800

    cadvisor

--- build.sh
+++ build.sh
@@ -3,17 +3,18 @@ buildNumber=${BUILD_NUMBER}
 docker rmi -f localhost:5000/devopsly200-app-teststage
 docker build -t localhost:5000/devopsly200-app-teststage -f DockerfileTest .
 
-docker rmi -f localhost:5000/devopsly200-app-teststage-failed
-docker build -t localhost:5000/devopsly200-app-teststage-failed -f DockerfileTest .
-
 docker rmi -f localhost:5000/devopsly200-app-stagingstage
 docker build -t localhost:5000/devopsly200-app-stagingstage -f DockerfileStaging .
-
-docker rmi -f localhost:5000/devopsly200-app-stagingstage-failed
-docker build -t localhost:5000/devopsly200-app-stagingstage-failed -f DockerfileStaging .
 
 docker rmi -f localhost:5000/devopsly200-app-productionstage
 docker build -t localhost:5000/devopsly200-app-productionstage -f DockerfileProduction .
 
-docker rmi -f localhost:5000/devopsly200-app-productionstage-failed
-docker build -t localhost:5000/devopsly200-app-productionstage-failed -f DockerfileProduction .
+marathon=$(curl http://myip.dnsomatic.com)
+echo ${marathon}
+
+echo Deleting cadvisor 
+curl -X DELETE -H "Content-Type: application/json" http://${marathon}:8080/v2/apps/cadvisor?force=true
+sleep 5
+echo posting app
+curl -X POST -H "Content-Type: application/json" http://${marathon}:8080/v2/apps -d@cadvisor_marathon.json
+echo Deployed to test
