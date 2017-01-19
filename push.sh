0f1afe3022d6915e2a6e8cdcb160b7a1876a652f
ramesh <ramesh@nirvaha.com>
2017-01-18 20:52:19 -0800

    misc

--- push.sh
+++ push.sh
@@ -1,8 +1,5 @@
 buildNumber=${BUILD_NUMBER}
 
 docker push localhost:5000/devopsly200-app-teststage
-docker push localhost:5000/devopsly200-app-teststage-failed
 docker push localhost:5000/devopsly200-app-stagingstage
-docker push localhost:5000/devopsly200-app-stagingstage-failed
 docker push localhost:5000/devopsly200-app-productionstage
-docker push localhost:5000/devopsly200-app-productionstage-failed
